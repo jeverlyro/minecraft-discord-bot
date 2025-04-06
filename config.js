28b8dfb66774e41e675d4c4440c530519bf2fc88
jeverlyro <ikadektresnajeverly78@gmail.com>
2025-04-07 07:09:21 +0800

    Update default port and status update interval in configuration

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // Default server settings
   defaultServer: 'play.uduality.site',
-  defaultPort: 19231,
+  defaultPort: 7000,
   
   // Bot settings
-  statusUpdateInterval: 0.5 * 60 * 1000, 
+  statusUpdateInterval: 1 * 60 * 1000, 
   statusChannelId: '1357623867824144435',
   embedColor: {
     online: '#00AA00',
